f213581904523b8141d9992e8bb4a1db2aaa1bdf
YooSangWoo <qswwsq11@gmail.com>
2023-01-26 16:42:13 +0900

    Initial commit

---
+++ lib/screen/custom_scroll_view_screen.dart
@@ -0,0 +1,192 @@
+import 'package:flutter/material.dart';
+import 'package:scrollable_widgets/const/colors.dart';
+
+class _SliverFixedHeaderDelegate extends SliverPersistentHeaderDelegate {
+  final Widget child;
+  final double maxHeight;
+  final double minHeight;
+
+  _SliverFixedHeaderDelegate({
+    required this.child,
+    required this.maxHeight,
+    required this.minHeight,
+  });
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return SizedBox.expand(
+      child: child,
+    );
+  }
+
+  // 최대 높이
+  @override
+  double get maxExtent => maxHeight;
+
+  // 최소 높이
+  @override
+  double get minExtent => minHeight;
+
+  // covariant - 상속된 클래스도 사용 가능 (그래서 _SliverFixedHeaderDelegate로 바꿈)
+  // oldDelegate - build가 실행이 됐을때 이전의 Delegate
+  // 새로운 Delegate - this
+  // shouldRebuild - 새로 build를 해야할지 말지 결정 /
+  // return false면 build 안함,  true면 build 다시함
+  @override
+  bool shouldRebuild(_SliverFixedHeaderDelegate oldDelegate) {
+    return oldDelegate.minExtent != minHeight ||
+        oldDelegate.maxExtent != maxHeight ||
+        oldDelegate.child != child;
+  }
+}
+
+class CustomScrollViewScreen extends StatelessWidget {
+  final List<int> numbers = List.generate(100, (index) => index);
+
+  CustomScrollViewScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          renderSliverAppbar(),
+          renderHeader(),
+          renderBuilderSliverList(),
+          renderHeader(),
+          renderSliverGridBuilder(),
+          renderBuilderSliverList()
+        ],
+      ),
+    );
+  }
+
+  SliverPersistentHeader renderHeader(){
+    return SliverPersistentHeader(
+      delegate: _SliverFixedHeaderDelegate(
+        child: Container(
+          color: Colors.black,
+          child: Center(
+            child: Text(
+              '신기하지~',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+        minHeight: 75,
+        maxHeight: 150,
+      ),
+    );
+  }
+
+  // AppBar
+  SliverAppBar renderSliverAppbar() {
+    return SliverAppBar(
+      // 스크롤 했을때 리스트의 중간에도 AppBar가 내려오게 할 수 있음.
+      floating: true,
+      // 완전 고정
+      pinned: false,
+      // 자석 효과
+      // floating: true 일때만 사용 가능
+      snap: true,
+      // 맨 위에서 한계 이상으로 스크롤 했을때
+      // 남는 공간을 차지
+      stretch: true,
+      expandedHeight: 200,
+      collapsedHeight: 150,
+      flexibleSpace: FlexibleSpaceBar(
+        background: Image.asset(
+          'asset/img/image_1.jpeg',
+          fit: BoxFit.cover,
+        ),
+        title: Text('FlexibleSpace'),
+      ),
+      title: Text('CustomScrollViewScreen'),
+    );
+  }
+
+  // ListView 기본 생성자와 유사함
+  SliverList renderChildSliverList() {
+    return SliverList(
+      delegate: SliverChildListDelegate(
+        numbers
+            .map(
+              (e) => renderContainer(
+                  color: rainbowColors[e % rainbowColors.length], index: e),
+            )
+            .toList(),
+      ),
+    );
+  }
+
+  // ListView.builder 생성자와 유사함
+  SliverList renderBuilderSliverList() {
+    return SliverList(
+      delegate: SliverChildBuilderDelegate((context, index) {
+        return renderContainer(
+          color: rainbowColors[index % rainbowColors.length],
+          index: index,
+        );
+      }, childCount: 100),
+    );
+  }
+
+  // GridView.count 와 유사함
+  SliverGrid renderChildSliverGrid() {
+    return SliverGrid(
+      delegate: SliverChildListDelegate(
+        numbers
+            .map(
+              (e) => renderContainer(
+                  color: rainbowColors[e % rainbowColors.length], index: e),
+            )
+            .toList(),
+      ),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+      ),
+    );
+  }
+
+  // GridView.builder 와 유사함
+  SliverGrid renderSliverGridBuilder() {
+    return SliverGrid(
+      delegate: SliverChildBuilderDelegate((context, index) {
+        return renderContainer(
+          color: rainbowColors[index % rainbowColors.length],
+          index: index,
+        );
+      }, childCount: 100),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 150,
+      ),
+    );
+  }
+
+  Widget renderContainer({
+    required Color color,
+    required int index,
+    double? height,
+  }) {
+    print(index);
+
+    return Container(
+      key: Key(index.toString()),
+      height: height == null ? 300 : height,
+      color: color,
+      child: Center(
+        child: Text(
+          index.toString(),
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w700,
+            fontSize: 30.0,
+          ),
+        ),
+      ),
+    );
+  }
+}
